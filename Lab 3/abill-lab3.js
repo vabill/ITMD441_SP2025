9ce924fe6a2258b25e05619c7390c53176d79838
vabill <vbill@hawk.iit.edu>
2025-03-04 17:14:20 -0600

    Update abill-lab3.js
    fresh start after revisitng

--- Lab 3/abill-lab3.js
+++ Lab 3/abill-lab3.js
@@ -2,8 +2,6 @@
 // ITMD 441 Undergraduate Student
 
 // Exercise #1
-function minMaxAverage(arr) {
-    var min = [0];
-    var max = [0];
-    var avg = [0]; // average
+function minMaxAverage(numbers) {
+    
 }
