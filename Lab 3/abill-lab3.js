f7cda931047d7ae5b34cfeaf13e45ebd122ccc3b
vabill <vbill@hawk.iit.edu>
2025-03-04 17:32:02 -0600

    Update abill-lab3.js
    had most of this already, forgot to update for last code before doing this

--- Lab 3/abill-lab3.js
+++ Lab 3/abill-lab3.js
@@ -16,3 +16,23 @@ function minMaxAverage(numbers) {
 minMaxAverage([2, 5, 23, 6, 9, 4, 30, 1]);
 minMaxAverage([1, 5, 3, 5, 10, 12, 8, 6]);
 minMaxAverage([12, 45, 1, 8, 4, 14, 24, 6]);
+
+// Exercise 2
+function countVowels(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    console.log('${str}: ${count} vowels');
+}
+
+//Tests
+countVowels("Winter");
+countVowels("Stray Kids");
+countVowels("Ateez");
+
